7f0af4b6e67bd8c1efb76ff5e8f69ce2656fb4fd
Harris Brakmic <brakmic@gmail.com>
2023-09-20 13:59:38 +0200

    fix: temporarily disable eslint checks for some files

---
+++ src/lib/record.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line
+export type Field = any
+
+export type ObjectHeaderItem = { id: string; title: string }
+export type ObjectStringifierHeader = ObjectHeaderItem[] | string[]
