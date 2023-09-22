acfae609c7364c9f020c73709cde2cff9e73a86a
Harris Brakmic <brakmic@gmail.com>
2023-09-22 08:38:29 +0200

    fix: tests

--- src/test/browser.test.ts
+++ src/test/browser.test.ts
@@ -1,16 +1,20 @@
 import { CsvWriter } from "../lib/csv-writer";
-import { CsvStringifierMock } from "./mocks/csv-stringifier.mock";
+import { FieldStringifier } from "../lib/field-stringifier";
+import { CsvStringifier } from "../lib/csv-stringifiers/abstract";
 import { FileWriterMock } from "./mocks/file-writer.mock";
+import { TestFieldStringifier, TestCsvStringifier } from "./impl/stringifiers";
 
 describe('CsvWriter Browser Compatibility', () => {
     let writer: CsvWriter<string[]>;
     let fileWriterMock: FileWriterMock;
-    let csvStringifierMock: typeof CsvStringifierMock;
+    let fieldStringifier: FieldStringifier;
+    let csvStringifier: CsvStringifier<string[]>;
 
     beforeEach(() => {
         fileWriterMock = new FileWriterMock();
-        csvStringifierMock = CsvStringifierMock;
-        writer = new CsvWriter<string[]>(csvStringifierMock, '', '', false, fileWriterMock);
+        fieldStringifier = new TestFieldStringifier(',');
+        csvStringifier = new TestCsvStringifier(fieldStringifier, '\n');
+        writer = new CsvWriter<string[]>(csvStringifier, '', '', false, fileWriterMock);
     });
 
     it('should write records without errors', async () => {
