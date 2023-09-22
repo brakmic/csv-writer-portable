bf7ba6ff2bb5e77303d45d573b3732122bd4fe54
Harris Brakmic <brakmic@gmail.com>
2023-09-22 08:48:14 +0200

    fix: linting errors

--- src/lib/csv-writer-factory.ts
+++ src/lib/csv-writer-factory.ts
@@ -1,50 +1,68 @@
-import {CsvWriter} from './csv-writer'
-import {CsvStringifierFactory} from './csv-stringifier-factory'
-import {ObjectStringifierHeader} from './record'
-import { ObjectMap } from './lang/object'
+import { CsvWriter } from './csv-writer';
+import { CsvStringifierFactory } from './csv-stringifier-factory';
+import { ObjectStringifierHeader } from './record';
+import { ObjectMap } from './lang/object';
 
 export interface ArrayCsvWriterParams {
-    path: string
-    header?: string[]
-    fieldDelimiter?: string
-    recordDelimiter?: string
-    alwaysQuote?: boolean
-    encoding?: string
-    append?: boolean
+    path: string;
+    header?: string[];
+    fieldDelimiter?: string;
+    recordDelimiter?: string;
+    alwaysQuote?: boolean;
+    encoding?: string;
+    append?: boolean;
 }
 
 export interface ObjectCsvWriterParams {
-    path: string
-    header: ObjectStringifierHeader
-    fieldDelimiter?: string
-    recordDelimiter?: string
-    headerIdDelimiter?: string
-    alwaysQuote?: boolean
-    encoding?: string
-    append?: boolean
+    path: string;
+    header: ObjectStringifierHeader;
+    fieldDelimiter?: string;
+    recordDelimiter?: string;
+    headerIdDelimiter?: string;
+    alwaysQuote?: boolean;
+    encoding?: string;
+    append?: boolean;
 }
 
 export class CsvWriterFactory {
-    constructor(private readonly csvStringifierFactory: CsvStringifierFactory) {}
+    constructor(
+        private readonly csvStringifierFactory: CsvStringifierFactory,
+    ) {}
 
-    createArrayCsvWriter(params: ArrayCsvWriterParams): CsvWriter<any[]> {
-        const csvStringifier = this.csvStringifierFactory.createArrayCsvStringifier({
-            header: params.header,
-            fieldDelimiter: params.fieldDelimiter,
-            recordDelimiter: params.recordDelimiter,
-            alwaysQuote: params.alwaysQuote
-        });
-        return new CsvWriter(csvStringifier, params.path, params.encoding, params.append);
+    createArrayCsvWriter<T = unknown>(
+        params: ArrayCsvWriterParams,
+    ): CsvWriter<T[]> {
+        const csvStringifier =
+            this.csvStringifierFactory.createArrayCsvStringifier({
+                header: params.header,
+                fieldDelimiter: params.fieldDelimiter,
+                recordDelimiter: params.recordDelimiter,
+                alwaysQuote: params.alwaysQuote,
+            });
+        return new CsvWriter<T[]>(
+            csvStringifier,
+            params.path,
+            params.encoding,
+            params.append,
+        );
     }
 
-    createObjectCsvWriter(params: ObjectCsvWriterParams): CsvWriter<ObjectMap<any>> {
-        const csvStringifier = this.csvStringifierFactory.createObjectCsvStringifier({
-            header: params.header,
-            fieldDelimiter: params.fieldDelimiter,
-            recordDelimiter: params.recordDelimiter,
-            headerIdDelimiter: params.headerIdDelimiter,
-            alwaysQuote: params.alwaysQuote
-        });
-        return new CsvWriter(csvStringifier, params.path, params.encoding, params.append);
+    createObjectCsvWriter<T = unknown>(
+        params: ObjectCsvWriterParams,
+    ): CsvWriter<ObjectMap<T>> {
+        const csvStringifier =
+            this.csvStringifierFactory.createObjectCsvStringifier({
+                header: params.header,
+                fieldDelimiter: params.fieldDelimiter,
+                recordDelimiter: params.recordDelimiter,
+                headerIdDelimiter: params.headerIdDelimiter,
+                alwaysQuote: params.alwaysQuote,
+            });
+        return new CsvWriter<ObjectMap<T>>(
+            csvStringifier,
+            params.path,
+            params.encoding,
+            params.append,
+        );
     }
 }
