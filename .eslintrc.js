22f9cd18781182c16c3f7f1809d2c73c49da9e53
Harris Brakmic <brakmic@gmail.com>
2023-09-22 07:53:49 +0200

    chore: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-throw-literal': 'error',
