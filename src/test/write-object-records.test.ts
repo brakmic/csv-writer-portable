1d6d462368c94932f3706b49a230243ceea23801
Harris Brakmic <brakmic@gmail.com>
2024-07-15 00:24:30 +0200

    test: add quote-empty-field tests

--- src/test/write-object-records.test.ts
+++ src/test/write-object-records.test.ts
@@ -11,6 +11,13 @@ describe('Write object records into CSV', () => {
         { name: 'Mary', lang: 'English' },
     ];
 
+    const recordsWithEmptyFields = [
+        ...records,
+        { name: 'Jack', lang: null },
+        { name: undefined, lang: 'German' },
+        { name: "Hank" }
+    ]
+
     describe('When only path and header ids are given', () => {
         const filePath = makeFilePath('minimum');
         // eslint-disable-next-line
@@ -171,4 +178,33 @@ describe('Write object records into CSV', () => {
             assertFile(filePath, 'NAME,COUNTRY\nBob,France\nMary,\n');
         });
     });
+
+    describe('When `quoteEmptyFields` flag is set', () => {
+        const filePath = makeFilePath('quote-empty-field')
+        const writer = createObjectCsvWriter({
+            path: filePath,
+            header: [{id: 'name', title: 'NAME'}, {id: 'lang', title: 'LANGUAGE'}],
+            quoteEmptyFields: true
+        })
+
+        it('quotes all empty fields', async () => {
+            await writer.writeRecords(recordsWithEmptyFields)
+            assertFile(filePath, 'NAME,LANGUAGE\nBob,French\nMary,English\nJack,""\n"",German\nHank,""\n')
+        })
+    })
+
+    describe('When `quoteEmptyFields` and `alwaysQuote` flag is set', () => {
+        const filePath = makeFilePath('quote-empty-field')
+        const writer = createObjectCsvWriter({
+            path: filePath,
+            header: [{id: 'name', title: 'NAME'}, {id: 'lang', title: 'LANGUAGE'}],
+            quoteEmptyFields: true,
+            alwaysQuote: true
+        })
+
+        it('quotes all empty fields', async () => {
+            await writer.writeRecords(recordsWithEmptyFields)
+            assertFile(filePath, '"NAME","LANGUAGE"\n"Bob","French"\n"Mary","English"\n"Jack",""\n"","German"\n"Hank",""\n')
+        })
+    })
 });
