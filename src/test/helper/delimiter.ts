e87905822a5bfd93a7b9b2709975b8b6b6b98591
Harris Brakmic <brakmic@gmail.com>
2024-07-14 22:53:52 +0200

    feat: import delimiters to test helper

--- src/test/helper/delimiter.ts
+++ src/test/helper/delimiter.ts
@@ -1,5 +1,6 @@
+import { VALID_FIELD_DELIMITERS } from "../../lib/field-stringifier";
 
 export const resolveDelimiterChar = (char: string) => {
-    if (char === ',' || char === ';') return char
-    throw new Error('Invalid field delimiter')
+    if (VALID_FIELD_DELIMITERS.includes(char)) return char;
+    throw new Error('Invalid field delimiter');
 }
